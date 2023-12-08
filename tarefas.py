4003d84fd15ee0554b9bbb83d0ef22d1620a3862
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2023-12-08 14:17:27 -0300

    Add files via upload

--- tarefas.py
+++ tarefas.py
@@ -1,9 +1,10 @@
 import notifica
+import Bancologin
 
-def tarefas(user_id):
+def tarefas(id_user):
     cod = True
     while cod:
-        comando = input("\ndigite o numero da opção \n 1 tarefa para voce\n 2 tarefa para seu amigo\n 3 para volta"
+        comando = input("\ndigite o numero da opção \n 1 tarefa para voce\n 2 tarefa para seu amigo\n 3 para Historico de tarefas\n Tarefa executada\n 5 para volta")
         if comando == "1":
             cod2 = True
             while cod2:
@@ -12,19 +13,43 @@ def tarefas(user_id):
                 comando3 = input()
                 if comando3 == "sim":
                     notifica.insert_notifica(id_user, comando2)
+                    Bancologin.alt_acumulador(7,id_user,comando2,"add")
                     return
         elif comando == "2":
             cod2 = True
             while cod2:
                 print()
                 comando2 = input("\nfavor didite a tarefa")
-                comando4 = input("\n digite o nome do seu amigo")
+
+                resposta = Bancologin.consulta(id_user)
+                lista = resposta[5]
+                for i in range(lista):
+                    print(i,lista[i])######################################
+                comando4 = input("\n digite o numero do seu amigo",int)
+                Bancologin.consultalogin(nome)
+                nome = lista[comando4]
+                responsta2 = Bancologin.consultalogin(nome)
+
                 print ("\nA tarefa é para seu amigo e é ", comando2 , "digite sim para continual\n")
                 comando3 = input()
                 if comando3 == "sim":
-                    notifica.insert_notifica(id_user, comando2)
+                    notifica.insert_notifica(responsta2, comando2)
+                    Bancologin.alt_acumulador(7,responsta2,comando2,"add")
                     return
         elif comando == "3":
+            resposta = Bancologin.consulta(id_user)
+            print(resposta[7])
+        elif comando == "5":
+            #################excluir tarefa
+            resposta = Bancologin.consulta(id_user)
+            lista = resposta[7]
+            for i in range(lista):
+                print(i,lista[i])######################################
+            cod = input("digite o numero da tarefa")
+            Bancologin.alt_acumulador(7,id_user,cod,"remove")
+        elif comando == "6":
             return
-        else
+        else:
             print("erro de digitação")
+        return
+        
