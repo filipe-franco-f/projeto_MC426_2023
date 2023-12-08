c98f734a77cb33eee5ca44719680d9ab40e6dabd
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2023-12-08 02:06:13 -0300

    adicionando funcionalidade central do apk

---
+++ menu.py
@@ -0,0 +1,36 @@
+import login
+import notifica
+import tarefas
+
+id_user = 0
+name_user = "user"
+def sobre():
+    print("\nEste é um aplicativo feito por Alunos da unicamp para o curso de engenghari de software\n")
+    return
+while id_user == 0:
+    print("Por favor digite o Comando entre: \n Login: \n Sobre:")
+    comando = input()
+    if comando=="Login" or comando=="login":
+        resposta = login.login()
+        id_user = resposta[0]
+    #elif comando == "cadastro" or comando == "cadadstro":
+    #    login.cadastro_senha()
+    elif comando=="Sobre" or comando=="sobre":
+        sobre()
+    else:
+        print("Comando Invalido")
+while True:
+    print("\n Bem vindo ", name_user,"\n Digite uma das opçẽos: \n Notificação\n Agenda\n Tarefas\n Logoff\n")
+    comando = input()
+    if comando == "agenda" or comando == "agenda":
+        #chamar agenda felipe#########################
+    elif comando == "nofificacao" or comando == "notificação" or comando = "Notificação"
+        notifica.notifica(id_user)
+    elif comando == "Tarefas" or comando == "tarefas":
+        tarefas.tarefas(id_user)
+    elif comando == "logoff" or comando == "Logoff":
+        id_user = 0
+        break
+    else:
+        print("\n Comando Invalido" )
+print("Logout efetuado com sucesso")
