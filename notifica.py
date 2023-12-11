7c82d7b45506c5dca600e47cd31a822ddf720ca7
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2023-12-10 21:00:04 -0300

    Add files via upload

--- notifica.py
+++ notifica.py
@@ -1,9 +1,20 @@
 import Bancologin
 def insert_notifica(id_user,msg):
     resposta = Bancologin.consulta(id_user)
+    Bancologin.alt_acumulador(7,id_user,msg,"add")
+    return
+
+def remove_notifica(id_user,msg):
+    resposta = Bancologin.consulta(id_user)
+    Bancologin.alt_acumulador(7,id_user,msg,"remove")
     return
 
 def notifica(id_user):
     resposta = Bancologin.consulta(id_user)
-    print("\n", resposta[5])
+    text = resposta[6].replace(" ??? ", "\n")
+    print("\n", text)
+    cod = input("deseja apagar tudo?(sim)")
+    if cod == "sim":   
+        Bancologin.alt_dado(6,id_user,"","deleteall")
+
     
