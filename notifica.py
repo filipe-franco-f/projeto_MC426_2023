746c9edd0579ab7d5738bba226dfecb5a91432f4
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2023-12-11 12:40:08 -0300

    Update notifica.py

--- notifica.py
+++ notifica.py
@@ -1,14 +1,4 @@
 import Bancologin
-def insert_notifica(id_user,msg):
-    resposta = Bancologin.consulta(id_user)
-    Bancologin.alt_acumulador(7,id_user,msg,"add")
-    return
-
-def remove_notifica(id_user,msg):
-    resposta = Bancologin.consulta(id_user)
-    Bancologin.alt_acumulador(7,id_user,msg,"remove")
-    return
-
 def notifica(id_user):
     resposta = Bancologin.consulta(id_user)
     text = resposta[6].replace(" ??? ", "\n")
